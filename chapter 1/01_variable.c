c55a53d4517c821cc263730a40a74f78f3107293
Dev Payal <pawarpayal353@gmail.com>
2026-08-05 18:36:51 +0530

    Add files via upload

---
+++ chapter 1/01_variable.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a;
+    a = 6;
+    printf("%d", a);
+    return 0;
+}
---
+++ chapter 1/04_practice.c
@@ -0,0 +1,8 @@
+//To calculate volume of cylinder 
+#include<stdio.h>
+int main(){
+    int r = 4;
+    int height = 6;
+    printf("The volume of cylinder with radius %d and height %d is %.2f",r,height,3.14*r*r*height);
+    return 0;
+}
