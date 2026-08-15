fc6add756d5ac005528ff0677a66f8aef841bb1c
Dev Payal <pawarpayal353@gmail.com>
2026-08-15 23:00:07 +0530

    Delete Chapter 5 directory

--- Chapter 5/01pointer.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a = 6;
-    int*b = &a;
-    printf("The addrress of a is %d\n",a);
-    printf("The address of a is %u\n",&a);
-    return 0;
-}
--- Chapter 5/05_practice.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main (){
-int a = 100;
-int*p= &a;
-*p = *p + 10;
-printf("%d",a);
-return 0;
-}
